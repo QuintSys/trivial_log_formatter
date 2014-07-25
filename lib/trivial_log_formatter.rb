0764086e1ace00920a8f96aee363bfb78305293f
qbantek <qbantek@gmail.com>
2014-07-25 10:12:25 -0400

    simpler call

--- lib/trivial_log_formatter.rb
+++ lib/trivial_log_formatter.rb
@@ -7,8 +7,6 @@ module TrivialLogFormatter
     FORMAT = "[%s#%d] %5s -- %s: %s\n"
 
     def call(severity, time, progname, msg)
-      return '' if msg.respond_to?(:empty?) ? empty? : !msg
-
       FORMAT % [format_datetime(time), $$, severity, progname, msg2str(msg)]
     end
 
