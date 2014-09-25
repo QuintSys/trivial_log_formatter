61ac9c7c39f2500c0ebdc86e78af8e827afd6b16
qbantek <qbantek@gmail.com>
2014-09-25 11:06:54 -0400

    added colors

--- lib/trivial_log_formatter.rb
+++ lib/trivial_log_formatter.rb
@@ -4,10 +4,16 @@ require 'logger'
 module TrivialLogFormatter
 
   class Formatter < ::Logger::Formatter
-    FORMAT = "[%s#%d] %5s -- %s: %s\n"
+    FORMAT = "\033[%sm[%s#%d] %5s -- %s: %s\033[0m\n"
+    SEVERITY_COLOR_MAP = {:debug => '0;37', :info => '32', :warn => '33', :error => '31', :fatal => '31', :unknown => '37'}
 
     def call(severity, time, progname, msg)
-      FORMAT % [format_datetime(time), $$, severity, progname, msg2str(msg)]
+      msg = '' if msg.nil?
+      FORMAT % [severity_to_color(severity), format_datetime(time), $$, severity, progname, msg2str(msg)]
+    end
+
+    def severity_to_color(severity)
+      SEVERITY_COLOR_MAP[severity.downcase.to_sym]
     end
 
   end
